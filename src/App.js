03b2748e627e3c932a4d0b2fb9290fe0c94a9604
roberto <robpalacios1@hotmail.com>
2021-01-06 16:08:50 -0500

    add useState for input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+
+      <input />
+      <button>Send Message</button>
+      {/** input field */}
+      {/** button */}
+      {/** messages themselves */}
     </div>
   );
 }
