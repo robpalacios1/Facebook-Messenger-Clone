daec2ec54732e70da8853823a077001c9d0456da
roberto <robpalacios1@hotmail.com>
2021-01-07 16:08:52 -0500

    add ternary conditional to user ang guest

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,19 +3,27 @@ import React from 'react'
 /***** Material-ui *****/
 import { Card, CardContent, Typography } from '@material-ui/core'
 
-const Message = (props) => {
+/***** CSS *****/
+import './Message.css'
+
+const Message = ({ message, username }) => {
+
+    const isUser = username === message.username;
+
     return (
-       <Card>
-            <CardContent>
-                <Typography
-                    color="white"
-                    variant="h5"
-                    component="h2"
-                >
-                    {props.username}: {props.text}
-                </Typography>
-            </CardContent>
-        </Card>
+        <div className={`message ${isUser && 'message__user'}`}>
+            <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+                <CardContent>
+                    <Typography
+                        color="white"
+                        variant="h5"
+                        component="h2"
+                    >
+                    {message.username}: {message.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
