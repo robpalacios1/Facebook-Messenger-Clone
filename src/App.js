514169f0fa947c00a740e030650abf9a34067b7c
roberto <robpalacios1@hotmail.com>
2021-01-06 23:47:54 -0500

    add useState messages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([])
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send a message
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Hello!</h1>
 
-      <input />
-      <button>Send Message</button>
+      <input
+        value={input}
+        onChange={event => setInput(event.target.value)}
+      />
+      <button onClick={() => sendMessage()}>Send Message</button>
       {/** input field */}
       {/** button */}
       {/** messages themselves */}
