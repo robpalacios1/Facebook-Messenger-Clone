b2048949f15b2b9818e22e0af1aee8829e4f18c7
roberto <robpalacios1@hotmail.com>
2021-01-09 11:57:19 -0500

    change from message.text to message.message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 /***** Material-ui *****/
 import { Card, CardContent, Typography } from '@material-ui/core'
@@ -6,12 +6,12 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 /***** CSS *****/
 import './Message.css'
 
-const Message = ({ message, username }) => {
+const Message = forwardRef(({ message, username }, ref) => {
 
     const isUser = username === message.username;
 
     return (
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card className={isUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent>
                     <Typography
@@ -19,12 +19,12 @@ const Message = ({ message, username }) => {
                         variant="h5"
                         component="h2"
                     >
-                    {message.username}: {message.text}
+                    {message.username}: {message.message}
                     </Typography>
                 </CardContent>
             </Card>
         </div>
     )
-}
+})
 
 export default Message
